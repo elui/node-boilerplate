94c849fa55c45da858a55b20ef034577e2784dde
Elliott Lui <elliott@duettoresearch.com>
2016-11-20 01:11:00 -0800

    Test ecosystem pm2 deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "Wanderwize",
+      script    : "server.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production : {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "ubuntu",
+      host : "wanderwize.com",
+      ref  : "origin/master",
+      repo : "git@github.com:elui/wanderwize.git",
+      path : "/home/ubuntu/wanderwize",
+      "post-setup" : "ls -la",
+      // "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production",
+      env : {
+        "MONGODB" : "mongodb://wanderwizeapp:prod@ds159217.mlab.com:59217/wanderwize",
+        "port" : 80
+      }
+    }
+  }
+}
